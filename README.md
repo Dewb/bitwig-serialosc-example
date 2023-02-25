dbc446a3af504c9941e7f93e04084b1f1561af78
Michael Dewberry <michael.dewberry@gmail>
2023-02-25 18:31:28 -0500

    simplify user experience with host.restart()

--- README.md
+++ README.md
@@ -9,12 +9,11 @@ Example of connecting to a [serialosc](https://monome.org/docs/serialosc/osc/) d
 3. Click **Add Controller** and select **monome** as **Hardware Vendor** and **serialosc** as **Product**. Click **Add**.
 4. In the controller entry that appears, click the gear icon to expose controls.
 5. Click **Detect Device** and, if a serialosc device is connected, the **Device Port** should update with your device port.
-6. Click the power button on the controller entry to disable it, and then click it a second time to restart.
+6. Turn an encoder (for arc) or press keys (for grid) and you should see LED feedback.
 
 ### Notes
 
-Restarting the extension is necessary because Bitwig requires extensions to set up their OSC connections during initialization,
-but serialosc expects clients to request a list of connected devices to discover OSC port assignments at runtime. Turning it
-off and back on allows the extension to connect to the port detected in the previous session.
+Bitwig requires extensions to set up their OSC connections during initialization, so the extension has to restart
+itself when the port is changed or a device is detected.
 
 Typescript dependencies are just to get API syntax completion in VS Code; no compilation step is necessary.
--- serialosc.control.js
+++ serialosc.control.js
@@ -1,13 +1,5 @@
 // Example of an approach to using Bitwig's Javascript Controller API
 // to connect to a serialosc device (grid or arc.)
-//
-// Bitwig requires that all OSC connections are set up during init(),
-// so we need separate invocations of the extension to query devices
-// and set up the device connection.
-//
-// Press the "detect device" button to identify an attached serialosc device
-// and set the "device port" preference to the device's port. Then disable
-// and re-enable the extension to connect to the device.
 
 loadAPI(16);
 
@@ -21,26 +13,44 @@ var listenerConnection = null;
 var portSetting = null;
 
 var serialOscPort = 12002;
-var listenerPort = 19991;
+var listenerPort = 19996;
+var devicePort = 0;
+
 var hostname = "127.0.0.1";
 var prefix = "/bitwig";
-
-var devices = {};
 
 // Variables to hold arc/grid LED state
 var encs = [0, 0, 0, 0];
 var keys = [...Array(16)].map(_ => Array(16).fill(0));
 
+var cursorTrack = null;
+var cursorDevice = null;
+var remoteControls = null;
+
 function init() {
+
     var oscModule = host.getOscModule();
     var preferences = host.getPreferences();
 
     // Create a preference for the UDP port for the device we want to connect to.
-    portSetting = preferences.getNumberSetting("Device Port", "Connection", 0, 65535, 1, "", 15000);
+    portSetting = preferences.getStringSetting("Device Port", "Connection", 5, "15000");
+    portSetting.addValueObserver(function(newValue) {
+        var v = parseInt(newValue);
+        if (isNaN(v) || (v != 0 && v < 1024) || v > 65535) {
+            // Ignore string that are not valid OSC ports, or 0 for "no device"
+            portSetting.set(devicePort.toString());
+        }
+        else if (parseInt(newValue) != devicePort) {
+            // Bitwig requires that all OSC connections are set up during init(),
+            // so we need to restart the extension whenever the port changes.
+            host.restart();
+        }
+    });
 
-    // Create a pushbutton that will request a list of connected devices from serialosc and autofill a valid port.
+    // Create a pushbutton that will request a list of connected devices from serialosc and autofill the correct port.
     var detect = preferences.getSignalSetting("Detect", "Connection", "Detect Device Port");
     detect.addSignalObserver(function() {
+        portSetting.set("0");
         if (serialOscConnection) {
             serialOscConnection.sendMessage("/serialosc/list", hostname, listenerPort);
         }
@@ -56,9 +66,27 @@ function init() {
     registerGridMethods(prefix, listenerAddressSpace);
 
     // Create our UDP/OSC connections: outgoing to serialosc and device, incoming to listener.
-    serialOscConnection = oscModule.connectToUdpServer(hostname, serialOscPort, null);
-    listenerConnection = oscModule.createUdpServer(listenerPort, listenerAddressSpace);
-    deviceConnection = connectToDevice(Math.round(portSetting.getRaw()));
+    try {
+        serialOscConnection = oscModule.connectToUdpServer(hostname, serialOscPort, null);
+    } catch(e) {
+        println("Got exception connecting to serialosc port " + serialOscPort + ": " + e.message);
+    }
+
+    try {
+        listenerConnection = oscModule.createUdpServer(listenerPort, listenerAddressSpace);
+    } catch(e) {
+        println("Got exception creating listener port " + listenerPort + ": " + e.message);
+    }
+
+    try {
+        devicePort = parseInt(portSetting.get());
+        if (devicePort != 0) {
+            deviceConnection = connectToDevice(devicePort);
+        }
+    } catch(e) {
+        println("Got exception connecting to device port " + devicePort + ": " + e.message);
+    }
+
 }
 
 function clamp(a, min, max) {
@@ -108,18 +136,23 @@ function onTilt(sensor, x, y, z) {
 }
 
 function onDeviceInfo(id, type, port) {
-    if (!(id in devices)) {
-        devices[id] = { id, type, port };
-
-        println(JSON.stringify(devices[id]));
-
+    if (port != devicePort) {
         if (portSetting) {
-            portSetting.setRaw(port);
+            portSetting.set(port.toString());
         }
+
+        // Bitwig requires that all OSC connections are set up during init(),
+        // so we need to restart the extension whenever the port changes.
+        host.restart();
     }
 }
 
 function connectToDevice(port) {
+    if (isNaN(port) || port < 1024 || port > 65535) {
+        println("Invalid port " + port);
+        return;
+    }
+
     println("Connecting to device on port " + port);
 
     var device = host.getOscModule().connectToUdpServer(hostname, port, host.getOscModule().createAddressSpace());
@@ -141,6 +174,7 @@ function registerGlobalMethods(addressSpace) {
         var id = args[0];
         var type = args[1];
         var port = args[2];
+        println("device detected: " + id + " type: " + type + " port: " + port);
 
         onDeviceInfo(id, type, port);
     });
@@ -152,7 +186,6 @@ function registerArcMethods(prefix, addressSpace) {
         var args = message.getArguments();
         var encoder = args[0];
         var delta = args[1];
-
         println("encoder " + encoder + " delta: " + delta);
 
         onEncoderDelta(encoder, delta);
